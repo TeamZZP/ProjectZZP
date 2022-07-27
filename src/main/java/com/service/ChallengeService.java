0a9c6c6e6b13b7852fb2a857bde8e0e39da44a87
choyeram <choyeram8@gmail.com>
2022-07-27 17:26:20 +0900

    Feature Challenge category

---
+++ src/main/java/com/service/ChallengeService.java
@@ -0,0 +1,49 @@
+package com.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.config.MySqlSessionFactory;
+import com.dao.ChallengeDAO;
+import com.dto.ChallengeDTO;
+
+public class ChallengeService {
+	
+	ChallengeDAO dao = new ChallengeDAO();
+
+	public int insertChallenge(ChallengeDTO dto) {
+		int n = 0;
+		SqlSession session = MySqlSessionFactory.getSqlSession();
+		try {
+			n = dao.insertChallenge(session, dto);
+			session.commit();
+		} finally {
+			session.close();
+		}
+		return n;
+	}
+
+	public List<ChallengeDTO> selectAllChallenge() {
+		List<ChallengeDTO> list = null;
+		SqlSession session = MySqlSessionFactory.getSqlSession();
+		try {
+			list = dao.selectAllChallenge(session);
+		} finally {
+			session.close();
+		}
+		return list;
+	}
+
+	public ChallengeDTO selectOneChallenge(int chall_id) {
+		ChallengeDTO dto = null;
+		SqlSession session = MySqlSessionFactory.getSqlSession();
+		try {
+			dto = dao.selectOneChallenge(session, chall_id);
+		} finally {
+			session.close();
+		}
+		return dto;
+	}
+
+}
