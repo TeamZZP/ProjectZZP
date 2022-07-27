c1ad8d7a1a8a4abbf206265fbad9cf699c12c50e
munbi <munbi0328@gmail.com>
2022-07-27 23:31:38 +0900

    Add ProductService ProducDAO ProductDTO

--- src/main/java/com/controller/product/ProductListServlet.java
+++ src/main/java/com/controller/product/ProductListServlet.java
@@ -1,25 +1,37 @@
 package com.controller.product;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ProductListServlet
- */
+import com.dto.ProductDTO;
+import com.service.ProductService;
+
+
 @WebServlet("/ProductListServlet")
 public class ProductListServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String p_category = request.getParameter("p_category");
+		System.out.println(p_category);
+		if(p_category == null) {
+			p_category = "main";
+		}
+		
+		ProductService service = new ProductService();
+		List<ProductDTO> list = service.productList(p_category);
+		
+		request.setAttribute("productList", list);
+		RequestDispatcher dis = request.getRequestDispatcher("main.jsp");
+		dis.forward(request, response);
+		
 	}
 
 	/**
