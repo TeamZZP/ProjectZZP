743588af9ccfd504d61dc7a01f1c42456d0556ae
munbi <munbi0328@gmail.com>
2022-08-04 18:26:40 +0900

    fix bug

--- src/main/java/com/controller/product/CategoryUIServlet.java
+++ src/main/java/com/controller/product/CategoryUIServlet.java
@@ -10,13 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class CategoryListServlet
  */
-@WebServlet("/CategoryListServlet")
-public class CategoryListServlet extends HttpServlet {
+@WebServlet("/CategoryUIServlet")
+public class CategoryUIServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int c_id = Integer.parseInt(request.getParameter("c_id"));
+		
+		response.sendRedirect("ProductServlet");
 		
 		
 	}
