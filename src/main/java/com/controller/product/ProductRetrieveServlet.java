38531aa0a5cff5fee4ecb1935536a3b73dfe5633
munbi <munbi0328@gmail.com>
2022-08-08 14:57:25 +0900

    update

--- src/main/java/com/controller/product/ProductRetrieveServlet.java
+++ src/main/java/com/controller/product/ProductRetrieveServlet.java
@@ -20,33 +20,33 @@ import com.service.ProductService;
  */
 @WebServlet("/ProductRetrieveServlet")
 public class ProductRetrieveServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
        
   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int p_id = Integer.parseInt(request.getParameter("p_id")) ; 
-		System.out.println("ProductRetrieveServlet에서 파싱한 p_id=="+p_id);
-		
-		ProductService service = new ProductService();
-		ProductDTO pdto = service.productRetrieve(p_id);
-		List <ImagesDTO> ilist = service.ImagesRetrieve(p_id);
-		
-		/* HttpSession session = request.getSession(); */
-		request.setAttribute("ProductRetrieveDTO", pdto);
-		request.setAttribute("ImagesRetrieveList", ilist);
-		
-		/* response.sendRedirect("productRetrieve.jsp"); */
-		
-		
-		  RequestDispatcher dis = request.getRequestDispatcher("productRetrieve.jsp");
-		  dis.forward(request, response);
-		 
-		
-	}
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      int p_id = Integer.parseInt(request.getParameter("p_id")) ; 
+      System.out.println("ProductRetrieveServlet에서 파싱한 p_id=="+p_id);
+      
+      ProductService service = new ProductService();
+      ProductDTO pdto = service.productRetrieve(p_id);
+      List <ImagesDTO> ilist = service.ImagesRetrieve(p_id);
+      
+      /* HttpSession session = request.getSession(); */
+      request.setAttribute("ProductRetrieveDTO", pdto);
+      request.setAttribute("ImagesRetrieveList", ilist);
+      
+      /* response.sendRedirect("productRetrieve.jsp"); */
+      
+      
+        RequestDispatcher dis = request.getRequestDispatcher("productRetrieve.jsp");
+        dis.forward(request, response);
+       
+      
+   }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doGet(request, response);
+   }
 
 }
