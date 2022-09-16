63687cd0558b23c4ed1e713d522dc74b80a11beb
choyeram <choyeram8@gmail.com>
2022-09-16 18:26:29 +0900

    Add a feature : show new info on admin dashboard

--- src/main/java/com/service/AdminService.java
+++ src/main/java/com/service/AdminService.java
@@ -1,12 +1,16 @@
 package com.service;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 
 import com.config.MySqlSessionFactory;
 import com.dao.AdminDAO;
+import com.dto.MemberDTO;
 import com.dto.PageDTO;
+import com.dto.ProductOrderDTO;
+import com.dto.QuestionDTO;
 
 public class AdminService {
 	
@@ -33,6 +37,39 @@ public class AdminService {
 			session.close();
 		}
 		return n;
+	}
+
+	public List<ProductOrderDTO> selectNewOrders(HashMap<String, String> map) {
+		List<ProductOrderDTO> list = null;
+		SqlSession session = MySqlSessionFactory.getSqlSession();
+		try {
+			list = dao.selectNewOrders(session, map);
+		} finally {
+			session.close();
+		}
+		return list;
+	}
+
+	public List<MemberDTO> selectNewMembers(HashMap<String, String> map) {
+		List<MemberDTO> list = null;
+		SqlSession session = MySqlSessionFactory.getSqlSession();
+		try {
+			list = dao.selectNewMembers(session, map);
+		} finally {
+			session.close();
+		}
+		return list;
+	}
+
+	public List<QuestionDTO> selectNewQuestion() {
+		List<QuestionDTO> list = null;
+		SqlSession session = MySqlSessionFactory.getSqlSession();
+		try {
+			list = dao.selectNewQuestion(session);
+		} finally {
+			session.close();
+		}
+		return list;
 	}
 
 }
